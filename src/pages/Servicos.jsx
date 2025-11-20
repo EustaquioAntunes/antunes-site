7e08e0834c18ac127af45cc216a88fad2c63f221
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 14:11:21 -0300

    Create Servicos.jsx

---
+++ src/pages/Servicos.jsx
@@ -0,0 +1,13 @@
+export default function Servicos() {
+  return (
+    <div>
+      <h1>Serviços</h1>
+      <ul>
+        <li>Projetos</li>
+        <li>Fiscalização</li>
+        <li>Comissionamento</li>
+        <li>Automação industrial</li>
+      </ul>
+    </div>
+  );
+}
