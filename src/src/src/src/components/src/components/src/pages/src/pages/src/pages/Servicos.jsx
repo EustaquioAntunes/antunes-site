516108b9d04945f1f9dac484dffe9e3493d7def9
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 12:11:17 -0300

    Create Servicos.jsx

---
+++ src/src/src/src/components/src/components/src/pages/src/pages/src/pages/Servicos.jsx
@@ -0,0 +1,33 @@
+export default function Servicos() {
+  return (
+    <div>
+      <h1>O que Fazemos</h1>
+
+      <p>Oferecemos soluções completas através de:</p>
+
+      <ul>
+        <li>Projetos</li>
+        <li>Fiscalização de montagem</li>
+        <li>Comissionamento</li>
+      </ul>
+
+      <h2>Áreas de Atuação</h2>
+      <ul>
+        <li>Elétrica industrial</li>
+        <li>Instrumentação</li>
+        <li>Automação</li>
+        <li>Sistemas de prevenção e combate a incêndio (AVCB)</li>
+      </ul>
+
+      <h2>Aplicações</h2>
+      <ul>
+        <li>Subestações</li>
+        <li>Fornos elétricos</li>
+        <li>Laminações</li>
+        <li>Transportadores e moinhos</li>
+        <li>Silos e sistemas de armazenagem</li>
+        <li>Paletizadoras</li>
+      </ul>
+    </div>
+  );
+}
