700c3a3012607f75ffdfbc9192d0dc967e7c5e7c
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 14:21:29 -0300

    Create Contato.jsx

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,12 @@
+export default function Contato() {
+  return (
+    <div>
+      <h1>Contato</h1>
+      <p>
+        Eng. Eustaquio Antunes<br />
+        Telefone: (31) 9 8260-7665<br />
+        E-mail: eustaquioasi@gmail.com
+      </p>
+    </div>
+  );
+}
