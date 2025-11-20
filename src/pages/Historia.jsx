3d49fb9d8fb169aa22523158cdb6d9e1518d9973
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 12:36:16 -0300

    Create Historia.jsx

---
+++ src/pages/Historia.jsx
@@ -0,0 +1,26 @@
+export default function Historia() {
+  return (
+    <div>
+      <h1>Nossa História</h1>
+      <p>
+        A Antunes Automação de Sistemas Industriais foi fundada em 2005 com o
+        objetivo de entregar serviços de excelência nas áreas de elétrica,
+        instrumentação e automação industrial.
+      </p>
+
+      <p>
+        Ao longo de sua trajetória, participou de projetos de grande relevância,
+        trazendo eficiência, padronização e tecnologia para ambientes
+        industriais complexos.
+      </p>
+
+      <h2>Valores</h2>
+      <ul>
+        <li>Excelência técnica</li>
+        <li>Segurança</li>
+        <li>Transparência</li>
+        <li>Eficiência energética</li>
+      </ul>
+    </div>
+  );
+}
