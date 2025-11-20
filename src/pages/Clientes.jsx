5aafbf2e04339945484c30d7cfc3f802838addd6
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 12:37:02 -0300

    Create Clientes.jsx

---
+++ src/pages/Clientes.jsx
@@ -0,0 +1,20 @@
+export default function Clientes() {
+  return (
+    <div>
+      <h1>Principais Clientes</h1>
+
+      <ul>
+        <li>Bunge</li>
+        <li>Unilever</li>
+        <li>Thyssenkrupp</li>
+        <li>KHD Humboldt Wedag</li>
+        <li>OFI – Olam Food Ingredients</li>
+      </ul>
+
+      <p>
+        Trabalhamos lado a lado com empresas que movem a economia, entregando
+        confiança, tecnologia e desempenho.
+      </p>
+    </div>
+  );
+}
