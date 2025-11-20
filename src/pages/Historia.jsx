eab91af63b25fa961e18bff0b81976c412f6fd9e
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 14:13:49 -0300

    Delete src/pages directory

--- src/pages/Historia.jsx
+++
@@ -1,8 +0,0 @@
-export default function Historia() {
-  return (
-    <div>
-      <h1>Nossa História</h1>
-      <p>Fundada em 2005, atuando em automação, elétrica e instrumentação.</p>
-    </div>
-  );
-}
