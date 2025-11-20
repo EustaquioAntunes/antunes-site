3bba44d7faf323939696858116ca6413238c29e6
EustaquioAntunes <eustaquioasi@gmail.com>
2025-11-20 14:10:56 -0300

    Create Historia.jsx

---
+++ src/pages/Historia.jsx
@@ -0,0 +1,8 @@
+export default function Historia() {
+  return (
+    <div>
+      <h1>Nossa História</h1>
+      <p>Fundada em 2005, atuando em automação, elétrica e instrumentação.</p>
+    </div>
+  );
+}
